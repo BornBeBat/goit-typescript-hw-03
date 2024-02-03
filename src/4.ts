c81dc7c48afc3a48c7cbb63cb727f052cc7bcbe6
Никита Шевченко <62301874+BornBeBat@users.noreply.github.com>
2024-02-03 15:34:38 +0200

    task 4

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,49 @@
-const key = new Key();
+class Key {
+    private signature: number
+    constructor() {
+        this.signature = Math.floor(Math.random() * 3);
+    }
 
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    constructor(private key: Key) {
+    }
+    getKey(): Key {
+        return this.key
+    }
+}
+
+abstract class Home {
+    door: boolean = false;
+    key: Key;
+
+    tenants: Person[] = [];
+    public comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person)
+        }
+    };
+    public abstract openDoor(key: Key): void
+}
+
+class MyHouse extends Home {
+       constructor(key: Key) {
+        super()
+        this.key = key
+    }
+    public openDoor(key: Key): void {
+        if (this.key.getSignature() === key.getSignature()) { this.door = true }
+    }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
-
 house.openDoor(person.getKey());
-
 house.comeIn(person);
 
-
-export {};
+export { };
